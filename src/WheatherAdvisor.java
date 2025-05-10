7db46207d7ad5ba1ccc59224a83c95a1ff62bf7d
Javier Moreno <jdmoreno26@outlook.com>
2025-05-09 23:20:27 -0500

    Desafio FizzBuzz mejora

--- src/WheatherAdvisor.java
+++ src/WheatherAdvisor.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class WheatherAdvisor {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Start of range: ");
+        int start = scanner.nextInt();
+
+        System.out.print("End of range: ");
+        int end = scanner.nextInt();
+
+        for (int i = start; i <= end; i++) {
+            if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
+                System.out.println("FizzBuzzBoom");
+            } else if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
             } else if (i % 3 == 0) {
                 System.out.println("Fizz");
@@ -11,5 +24,7 @@ public class WheatherAdvisor {
                 System.out.println(i);
             }
         }
+
+        scanner.close();
     }
 }
